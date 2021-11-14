9d1dbd39473cca68a3b7adbe7814af2d8a758bf6
Mohammad Wahiduzzaman <mwahiduz@my.centennialcollege.ca>
2021-11-14 12:03:51 -0500

    Database added

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+/*
+Author: Multiverse Design
+Date: Nov-11-2021
+FileName : db.js
+Where to connect from
+*/
+
+
+module.exports = {
+    "URI": "mongodb://localhost/survey"
+        //" Mongo Atlas link connection will go here"
+}
