761184b35d1fb368ab53450c9feca5cc166bfd01
fasop4 <fasop4@gmail.com>
2021-11-15 04:00:16 -0500

    fix commits to clean commits on general

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -14,4 +14,34 @@ module.exports.displayHomePage = (req, res, next) => {
         title: 'Home',
         page: 'home'
     });
+}Author: Multiverse Design
+Date: Nov-11-2021
+FileName : index.js
+*/
+
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+module.exports.displayHomePage = (req, res, next) => {
+
+    res.render('index', {
+        title: 'Home',
+        page: 'home'
+    });
+}Author: Multiverse Design
+Date: Nov-11-2021
+FileName : index.js
+*/
+
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+module.exports.displayHomePage = (req, res, next) => {
+
+    res.render('index', {
+        title: 'Home',
+        page: 'home'
+    });
 }
