9e83c1af427efdbea998a3720010af314d6d2c8d
Shahriar Tanvir <shahriartanvir75@gmail.com>
2021-11-14 23:41:45 -0500

    functional application

---
+++ server/routes/user.js
@@ -0,0 +1,30 @@
+
+
+
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//let jwt = require('jsonwebtoken');
+
+let passport = require('passport');
+
+
+
+let userController = require('../controllers/user');
+
+
+/* GET Route show the login page*/
+router.get('/login', userController.displayLoginPage);
+
+/* POST Route show the login page*/
+router.post('/login', userController.processLoginPage);
+
+
+/* GET Route show the register page */
+router.get('/register', userController.displayRegisterPage);
+
+/* POST  register page */
+router.post('/register', userController.processRegisterPage);
+
+module.exports = router;
