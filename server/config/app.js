991b1aa48c45970481bc68a5d0524f14dc3dddc3
fasop4 <fasop4@gmail.com>
2021-11-15 03:37:45 -0500

    "fixed conflict files"

--- server/config/app.js
+++ server/config/app.js
@@ -30,7 +30,6 @@ let indexRouter = require('../routes/index');
 let surveyRouter = require('../routes/survey');
 //let userRouter = require('../routes/user'); //TODO not needed yet. it is going be implemented later.
 
-
 let app = express();
 
 // view engine setup
