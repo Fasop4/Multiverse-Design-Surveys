8cc9d4510dc063dfef44c2b5477594c1fef18065
Mohammad Wahiduzzaman <mwahiduz@my.centennialcollege.ca>
2021-12-09 01:20:42 -0500

    survey question created

--- server/models/survey.js
+++ server/models/survey.js
@@ -1,64 +1,20 @@
-/*
-Author: Multiverse Design
-Date: Nov-12-2021
-FileName : survey.js
+//Mohammad Wahiduzzaman
 
-*/
 let mongoose = require('mongoose');
 
-/*let templateSurveyModel = new mongoose.Schema ({
-    author: {
-        type: String,
-        default: '',
-        trim: true,
-    }, 
-    surveyName: {
-        type: String,
-        default: '', 
-        trim: true,
-    },
-    description: {
-        type: String,
-        default: ''
-    },
-    questions: {
-        question1: {
-            type: String,
-            default: ''
-        },
-        question2: {
-            type: String,
-            default: ''
-        },
-        question3: {
-            type: String,
-            default: ''
-        },
-    },
-    Answers: {
-        answerQ1: String,
-        answerQ2: String,
-        answerQ3: String,
-    },
-    ExpDate: {
-        type: Date,
-    }
-});
-*/
-
 //create a model class
-let surveyModel = mongoose.Schema({ 
-    author: String, 
+
+let SurveyModel = new mongoose.Schema({
+    username: String,
     surveyName: String,
+    displayName: String,
+    surveyType: String,
     description: String,
-    questions: {
-        type: [String],
-    },
-    enterQuestion1: String,
-    enterQuestion2: String,
-    enterQuestion3: String
+    question: [String],
+    startDate: Date,
+    endDate: Date,
 }, {
     collection: "survey"
 });
 
-module.exports = mongoose.model('Survey', surveyModel);
+module.exports = mongoose.model('Survey', SurveyModel);
